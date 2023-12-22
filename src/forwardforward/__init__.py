6ee692816a61e56963ac1a5e4627d34465c45906
harryjulian <harryjulian16@gmail.com>
2023-12-22 16:18:05 +0000

    Update

--- src/forwardforward/__init__.py
+++
@@ -1,7 +0,0 @@
-from .config import *
-from .data import *
-from .experimental import *
-from .network import *
-from .run import *
-from .train import *
-from .viz import *
--- tests/_fixtures.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from forwardforward.data import load
-
-@pytest.fixture
-def mnist():
-  return load()
